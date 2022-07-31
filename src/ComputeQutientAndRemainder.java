feda9bee9c421cb3d42cb3e691fad3cfea2d6d43
Prabhas3699 <prabhak620@gmail.com>
2022-07-31 18:43:48 +0530

    Compute quotient and remainder

---
+++ src/ComputeQutientAndRemainder.java
@@ -0,0 +1,14 @@
+
+public class ComputeQutientAndRemainder {
+
+	public static void main(String[] args) {
+		
+		int n=10; //divident
+		int i=5; //divisor
+		
+		System.out.println("Quotient of the values ="+n/i);
+		System.out.println("Remainder of the values ="+n%i);
+
+	}
+
+}
