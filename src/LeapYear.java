fde3623a467b2690e87c272cc86c981a263fb696
Prabhas3699 <prabhak620@gmail.com>
2022-07-31 17:31:48 +0530

    given year is leap year or not

---
+++ src/LeapYear.java
@@ -0,0 +1,18 @@
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		
+int year = 2020;
+		
+		if (year%4 == 0 && year%100 !=0 || year%400 == 0 ) {
+		//if (year%4 == 0 || year%100 ==0 && year%400 == 0 ) {
+		//if (year%4 == 0 || year%100 ==0 || year%400 == 0 ) {
+			System.out.println("The Year is Leap Year");
+		}else {
+			System.out.println("The Year is not a Leap Year");
+		}
+
+	}
+
+}
