2cd883547a3ced1816e45700ee4727f4748f170b
Prabhas3699 <prabhak620@gmail.com>
2022-07-31 18:46:16 +0530

    program for swap two numbers

---
+++ src/SwapNumbers.java
@@ -0,0 +1,30 @@
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 4;
+		
+		System.out.println("a =" +a);
+		System.out.println("b =" +b);
+		
+		System.out.println("---Swap No.---");
+		{
+		/*int c;
+		
+		c = a + b;
+		b = c - b;
+		a = c - a;
+		*/
+			a=a+b;
+			b=a-b;
+			a=a-b;
+			
+		System.out.println("a =" +a);
+		System.out.println("b =" +b);
+		}
+
+	}
+
+}
