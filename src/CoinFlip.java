25cbde9f523bb0cbfaff49fa2464ca20709098a8
Prabhas3699 <prabhak620@gmail.com>
2022-07-31 17:28:18 +0530

    Coin flip gives head or tail

---
+++ src/CoinFlip.java
@@ -0,0 +1,17 @@
+
+public class CoinFlip {
+
+	public static void main(String[] args) {
+		
+		double number=Math.floor(Math.random()*2);
+		double i=0;
+		System.out.println(number);
+		
+		if(i<number) {
+			System.out.println("The Coin Flip is Tail");
+		}else{
+			System.out.println("The Coin Flip is Head");
+			}
+	}
+
+}
